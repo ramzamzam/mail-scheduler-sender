e374ab1f3aafd7d094854ae63a37d432dc1cb806
Yevheniy Miropolets <yevheniy.miropolets@gmail.com>
2017-07-09 21:22:54 +0300

    updates for connecting with master server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request-promise');
+const request = require('request');
 const config = require('./config.json');
 const app = express();
 
@@ -11,7 +11,6 @@ const transport = mailer.createTransport(config.transport);
 
 function networkLagMiddleware(req, res, next) {
     const time = (Math.random() * 3000).toFixed(0);
-    console.log({time});
     setTimeout(function() {
         return next();
     }, time);
@@ -28,7 +27,7 @@ app.post('/send', (req, res) => {
         from: 'yevheniy.miropolets@gmail.com',
         text: text
     };
-    transport.sendMail(mail, (err, info) => {
+    transport.sendMail(mail, (err) => {
         if(err) return res.status(400).end(err.toString());
         return res.status(200).end();
     });
@@ -37,14 +36,19 @@ app.post('/send', (req, res) => {
 app.listen(config.PORT, function(err) {
     if(err) console.error(err);
     console.log(`sending_agent listening on port ${config.PORT}`);
-    request(`${config.MASTER_URI}/register/sender`)
-        .then((response) => {
-            console.log('connected to master')
-        })
-        .catch(err => {
+    const options = {
+        url : `${config.MASTER_URI}/register/sender`,
+        headers: {
+            'x-port' : config.PORT
+        }
+    };
+    request(options, function (err) {
+        if(err) {
             console.error('error connecting to master');
             console.error(err);
+            return;
+        }
+        console.log('connected to master')
 
-        })
-
+    });
 });
